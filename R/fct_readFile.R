96f4dd7f25532346b6af5a6f34c72388d502eda2
nvelden <n.s.j.vandervelden@gmail.com>
2021-08-18 16:09:39 +0200

    readFile update to work with read_csv()

--- R/fct_readFile.R
+++ R/fct_readFile.R
@@ -9,6 +9,7 @@
 #' @importFrom readr read_lines read_csv
 #' @export
 readFile <- function(file) {
+  
   File <- list(
     PDB = NULL,
     structure = NULL,
@@ -38,39 +39,38 @@ readFile <- function(file) {
   NGL <- readr::read_lines(file)
   File$fileExt <- "pdb"
   File$PDB <- NGL
-  
   # Get file start
   structure_start <- grep(pattern = "#STRUCTURE", x = NGL)
   surface_start <- grep(pattern = "#SURFACE", x = NGL)
   ligand_start <- grep(pattern = "#LIGAND", x = NGL)
   sel_start <- grep(pattern = "#SELECTIONS", x = NGL)
-  
   labels_start <- grep(pattern = "#LABELS", x = NGL)
   contacts_start <- grep(pattern = "#CONTACTS", x = NGL)
   stage_start <- grep(pattern = "#STAGE", x = NGL)
   PDB_start <- grep(pattern = "#PDB", x = NGL)
   
+  
   # load data frames
   if(surface_start - structure_start > 1){
-  File$structure <- data.frame(readr::read_csv(NGL, skip = structure_start, n_max = surface_start - (structure_start + 2)))
+    File$structure <- data.frame(readr::read_csv(file, skip = structure_start, n_max = surface_start - (structure_start + 2)))
   }
   if(ligand_start - surface_start > 1){
-  File$surface <- data.frame(readr::read_csv(NGL, skip = surface_start, n_max = ligand_start - (surface_start + 2)))
+    File$surface <- data.frame(readr::read_csv(file, skip = surface_start, n_max = ligand_start - (surface_start + 2)))
   }
   if(stage_start - ligand_start > 1){
-  File$ligand <- data.frame(readr::read_csv(NGL, skip = ligand_start, n_max = stage_start - (ligand_start + 2)))
+    File$ligand <- data.frame(readr::read_csv(file, skip = ligand_start, n_max = stage_start - (ligand_start + 2)))
   }
   if(sel_start - stage_start > 1){
-  File$stage <- data.frame(readr::read_csv(NGL, skip = stage_start, n_max = sel_start - (stage_start + 2)))
+    File$stage <- data.frame(readr::read_csv(file, skip = stage_start, n_max = sel_start - (stage_start + 2)))
   }
   if ((labels_start - (sel_start +1)) > 1) {
-    File$selections <- data.frame(readr::read_csv(NGL, skip = sel_start, n_max = labels_start - (sel_start + 2)))
+    File$selections <- data.frame(readr::read_csv(file, skip = sel_start, n_max = labels_start - (sel_start + 2)))
   }
   if ((contacts_start - (labels_start + 1)) > 1) {
-    File$labels <- data.frame(readr::read_csv(NGL, skip = labels_start, n_max = contacts_start - (labels_start + 2)))
+    File$labels <- data.frame(readr::read_csv(file, skip = labels_start, n_max = contacts_start - (labels_start + 2)))
   }
   if ((PDB_start - (contacts_start + 1)) > 1) {
-    File$contacts <- data.frame(readr::read_csv(NGL, skip = contacts_start, n_max = PDB_start - (contacts_start + 2)))
+    File$contacts <- data.frame(readr::read_csv(file, skip = contacts_start, n_max = PDB_start - (contacts_start + 2)))
   }
   return(File)
 }
